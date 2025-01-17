52ae6a555135cf2bba10e0d8ce319ac081090960
Sidra Noor <sidranoor43210@gmail.com>
2025-01-17 12:46:39 +0500

    Card Section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import "./index.css"
 import Hero from "./components/Hero/Hero"
-
-
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Products from "./components/Products/Products";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import TopProducts from "./components/TopProducts/TopProducts";
 const App=()=> {
-  return <div className="">
+ React.useEffect(()=>{
+  AOS.init({
+    offset:100,
+    duration:800,
+    easing: "ease-in-sine",
+    delay:100,
+  });
+  AOS.refresh();
+},[]);
+  return <div>
 <Navbar/>
 <Hero/>
+<Products/>
+<TopProducts/>
   </div>;
 };
 
