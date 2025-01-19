61151e8646be092fcb0b5e1161cce1b431ccb829
Sidra Noor <sidranoor43210@gmail.com>
2025-01-19 17:37:49 +0500

    Shopify Complete Responsive Ui

--- src/App.js
+++ src/App.js
@@ -14,36 +14,36 @@ import Testimonial from "./components/Testimonial/Testimonial";
 import Footer from "./components/Footer/Footer";
 import Popup from "./components/Popup/Popup";
 
-const App=()=> {
+const App = () => {
 
-const [orderPopup, setOrderPopup]=React.useState
-(false);
-const handleOrderPopup=()=>{
-  setOrderPopup(!orderPopup);
-};
+  const [orderPopup, setOrderPopup] = React.useState
+    (false);
+  const handleOrderPopup = () => {
+    setOrderPopup(!orderPopup);
+  };
 
- React.useEffect(()=>{
-  AOS.init({
-    offset:100,
-    duration:800,
-    easing: "ease-in-sine",
-    delay:100,
-  });
-  AOS.refresh();
-},[]);
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
 
   return <div className="bg-white dark:bg-gray-900
   dark:text-white duration-200">
-<Navbar handleOrderPopup={handleOrderPopup}/>
-<Hero  handleOrderPopup={handleOrderPopup}/>
-<Products/>
-<TopProducts  handleOrderPopup={handleOrderPopup}/>
-<Banner/>
-<Subscribe/>
-<Products/>
-<Testimonial/>
-<Footer/>
-<Popup orderPopup={orderPopup} setOrderPopup={setOrderPopup}/>
+    <Navbar handleOrderPopup={handleOrderPopup} />
+    <Hero handleOrderPopup={handleOrderPopup} />
+    <Products />
+    <TopProducts handleOrderPopup={handleOrderPopup} />
+    <Banner />
+    <Subscribe />
+    <Products />
+    <Testimonial />
+    <Footer />
+    <Popup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
   </div>;
 };
 
